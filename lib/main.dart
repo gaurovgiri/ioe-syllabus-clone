c66465d0bd2e4905a3e0d3a11612a4b058b37a48
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2023-06-19 22:44:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home:ioe();
+  ));
+}
+class ioe extends StatefulWidget {
+  const ioe({Key? key}) : super(key: key);
+
+  @override
+  State<ioe> createState() => _ioeState();
+}
+
+class _ioeState extends State<ioe> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title:Text("Ioe-Syllabus app"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.notifications))
+        ],
+      ),
+    );
+  }
+}

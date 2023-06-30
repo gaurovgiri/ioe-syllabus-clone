11e71d5c880031446a10ee7ca256572306766df8
gaurovgiri <gaurovgiri@gmail.com>
2023-06-30 23:04:59 +0545

    Feat: add switch for dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:ioe_syllabus_clone/config/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'features/presentation/app_bar_design.dart';
 
 void main() {
-  runApp(IoeSyllabusClone());
+  runApp(ChangeNotifierProvider(
+    create: (_) => ThemeProvider(),
+    child: const IoeSyllabusClone(),
+  ));
 }
 
 class IoeSyllabusClone extends StatelessWidget {
@@ -11,6 +16,11 @@ class IoeSyllabusClone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: AppBarDesign());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: Provider.of<ThemeProvider>(context).currentThemeMode,
+        home: const AppBarDesign());
   }
 }

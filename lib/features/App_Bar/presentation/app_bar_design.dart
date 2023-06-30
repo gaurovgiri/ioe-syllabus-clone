9c6c25dc9cf36efee2bafc222f371c325fcde42f
gaurovgiri <gaurovgiri@gmail.com>
2023-06-30 23:04:59 +0545

    Feat: add switch for dark theme

--- lib/features/App_Bar/presentation/app_bar_design.dart
+++ lib/features/App_Bar/presentation/app_bar_design.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:ioe_syllabus_clone/config/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class AppBarDesign extends StatelessWidget {
   const AppBarDesign({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -22,7 +25,10 @@ class AppBarDesign extends StatelessWidget {
               icon: const Icon(
                 Icons.notifications,
                 color: Colors.deepOrange,
-              ))
+              )),
+          Switch(
+              value: themeProvider.currentThemeMode == ThemeMode.dark,
+              onChanged: (_) => themeProvider.toggleTheme())
         ],
       ),
     );
